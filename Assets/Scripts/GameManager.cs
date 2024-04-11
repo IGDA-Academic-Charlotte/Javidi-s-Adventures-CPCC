53c1959029c82d99acb896ef4ffaa6d0165be69f
Hallie Uczen <109750137+ShadowChaser000@users.noreply.github.com>
2024-04-11 02:13:33 -0400

    Menus and Game Manager
    Added Menus and Game Manager, tweaked scripting and build settings to match

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    //[Header("UI Elements")]
+
+
+    //[Header("Player Info")]
+
+
+    //[Header("Needs Script")]
+
+}
