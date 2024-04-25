15dce904d4e88aea80a0cfae016eabdf905e4013
otterpoprocks <grrrebelsoul@gmail.com>
2024-04-24 20:59:37 -0400

    making lava prefab

--- Assets/Scripts/LavaManager.cs
+++ Assets/Scripts/LavaManager.cs
@@ -10,23 +10,19 @@ public class LavaManager : MonoBehaviour
 
     void Start()
     {
-        // Get the Renderer component attached to the plane GameObject
         rend = GetComponent<Renderer>();
     }
 
     void Update()
     {
-        // Move the plane vertically
         transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 
     void LateUpdate()
     {
-        // Calculate the new texture offset based on the scroll speed
         float offsetX = Time.time * scrollSpeed;
         Vector2 uvOffset = new Vector2(offsetX, 0);
 
-        // Set the texture offset on the plane material
         rend.material.SetTextureOffset(textureName, uvOffset);
     }
 }
