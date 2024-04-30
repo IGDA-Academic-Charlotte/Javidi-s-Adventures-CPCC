30fbb7b7468801eb7feaf777afde9f239b04f67d
Mason <masonfilbrandt@gmail.com>
2024-04-30 17:18:19 -0400

    Reapply "Connected Pumice to Lava"
    This reverts commit 0a533e829d7c59a9c58f81df9d1056cafe5f246b.

--- Assets/Scripts/Pumice.cs
+++ Assets/Scripts/Pumice.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class Pumice : MonoBehaviour
 {
+    [SerializeField]
+    private bool useGlobalLavaPlane = true;
+    private GameObject globalLavaPlane;
+    private float globalLavaPlaneHeight;
+
     private bool playerIsTouching = false;
+
     private string state = "Stationary";
     private float wobbleTime = 0f;
     private float sinkCountdown = 0f;
@@ -13,15 +19,29 @@ public class Pumice : MonoBehaviour
     private float sinkDepth = 1f;
     private float submergedCountdown = 0f;
     private float submergedCountdownMax = 3f;
-    private Vector3 initialPosition;
+
+    private Vector3 anchorPosition;
+    private Vector3 offsetPosition = Vector3.zero;
 
     private void Start()
     {
-        initialPosition = transform.position;
+        if(useGlobalLavaPlane)
+        {
+            globalLavaPlane = GameObject.Find("Lava");
+        }
+        anchorPosition = transform.position;
     }
 
     private void Update()
     {
+        if(useGlobalLavaPlane)
+        {
+            globalLavaPlaneHeight = globalLavaPlane.transform.position.y;
+            if(globalLavaPlaneHeight >= anchorPosition.y)
+            {
+                anchorPosition.y = globalLavaPlaneHeight;
+            }
+        }
         switch(state)
         {
             case "Stationary":
@@ -34,14 +54,14 @@ public class Pumice : MonoBehaviour
                 break;
             case "Wobbling":
                 wobbleTime += 0.1f;
-                transform.position += Vector3.down * Mathf.Sin(wobbleTime) * Time.deltaTime;
+                offsetPosition += Vector3.down * Mathf.Sin(wobbleTime) * Time.deltaTime;
                 sinkCountdown -= 1f * Time.deltaTime;
                 if(sinkCountdown <= 0f) state = "Sinking";
                 break;
 
             case "Sinking":
-                transform.position += Vector3.down * Time.deltaTime;
-                if(transform.position.y <= initialPosition.y - sinkDepth)
+                offsetPosition += Vector3.down * Time.deltaTime;
+                if(offsetPosition.y <= -sinkDepth)
                 {
                     state = "Sunk";
                     submergedCountdown = submergedCountdownMax;
@@ -54,13 +74,15 @@ public class Pumice : MonoBehaviour
                 break;
 
             case "Rising":
-                transform.position += Vector3.up * Time.deltaTime;
-                if(transform.position.y >= initialPosition.y)
+                offsetPosition += Vector3.up * Time.deltaTime;
+                if(offsetPosition.y >= 0)
                 {
                     state = "Stationary";
                 }
                 break;
         }
+
+        transform.position = anchorPosition + offsetPosition;
     }
 
     private void OnTriggerEnter(Collider other)
