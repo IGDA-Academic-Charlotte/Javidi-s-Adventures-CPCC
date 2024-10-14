ddacec1ed3ffb953f67c266374bc4845531a3a10
Mason <masonfilbrandt@gmail.com>
2024-10-13 22:08:26 -0400

    Level 0 | Move Linear
    - Isolated Level 0 into its own scene
    - Improved Move Linear script

--- Assets/Scripts/MoveLinear.cs
+++ Assets/Scripts/MoveLinear.cs
@@ -7,30 +7,40 @@ public class MoveLinear : MonoBehaviour
     public Transform movingObject;
     public Transform goalTransform;
     public float moveSpeed = 1.0f;
+    public AudioClip moveSound;
 
     private Rigidbody movingObjectRigidBody;
+    private AudioSource audioSource;
     float moveSpeedMultiplier = 0.01f;
     bool moving = false;
+    private Vector3 distanceToGoal;
+    private Vector3 remainingDistance;
+    private Vector3 movementIncrement;
 
     // Start is called before the first frame update
     void Start()
     {
         movingObjectRigidBody = movingObject.GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+        distanceToGoal = goalTransform.position - transform.position;
+        remainingDistance = distanceToGoal;
+        movementIncrement = distanceToGoal.normalized * moveSpeed * moveSpeedMultiplier;
     }
 
     private void FixedUpdate()
     {
         if(moving)
         {
-            Vector3 distanceToGoal = goalTransform.position - movingObject.position;
-            Vector3 movementIncrement = distanceToGoal.normalized * moveSpeed * moveSpeedMultiplier;
-            if(distanceToGoal.magnitude >= movementIncrement.magnitude)
+            if(movementIncrement.magnitude <= remainingDistance.magnitude)
             {
                 movingObjectRigidBody.MovePosition(movingObject.position + movementIncrement);
+                remainingDistance -= movementIncrement;
             }
             else
             {
-                movingObjectRigidBody.MovePosition(goalTransform.position);
+                movingObjectRigidBody.MovePosition(movingObject.position + remainingDistance);
+                moving = false;
             }
         }
     }
@@ -38,5 +48,6 @@ public class MoveLinear : MonoBehaviour
     public void Activate()
     {
         moving = true;
+        if(moveSound != null) audioSource.PlayOneShot(moveSound);
     }
 }
